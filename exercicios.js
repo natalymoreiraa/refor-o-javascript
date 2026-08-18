28cb40f9b9afd9fb569e364a4694febd7fa7b396
nataly <nataly_moreira@estudante.sesisenai.org.br>
2026-08-18 16:24:38 -0300

    exercicio 4 feito

--- exercicios.js
+++ exercicios.js
@@ -60,7 +60,16 @@ if (numeroParImpar % 2 === 0) {
 // Se a média for >= 7, mostre "Aprovado".
 // Se não, mostre "Reprovado".
 // ------------------------------------------------------------
-
+let nota1 = 8;
+let nota2 = 6;
+let nota3 = 7;
+let media = (nota1 + nota2 + nota3) / 3;
+console.log(media);
+if (media >= 7) {
+  console.log("Aprovado");
+} else {
+  console.log("Reprovado");
+}
 
 
 
